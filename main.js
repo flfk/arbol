b2bb6b992cd94117740cba10819040d30ee26165
Michael Yang <michaelyang182@gmail.com>
2018-01-09 17:39:16 +1100

    added static stats and cross icon

--- main.js
+++ main.js
@@ -3,16 +3,18 @@
 var height = '70px';
 var iframe = document.createElement('iframe');
 iframe.src = chrome.extension.getURL('toolbar.html');
+iframe.id = 'myOwnCustomFirstToolbar1234'
 iframe.style.height = height;
-iframe.style.width = '100%';
+/*iframe.style.width = '100%';
 iframe.style.position = 'fixed';
 iframe.style.bottom = '0';
 iframe.style.left = '0';
 iframe.style.zIndex = '938089';
 iframe.style.backgroundColor = '#F4F4F4';
 iframe.style.border = 'thin solid #CCCCCC'
-document.documentElement.appendChild(iframe);﻿
+*/document.documentElement.appendChild(iframe);﻿
 document.body.style.padding = '0px 0px '+height; //set margin height
+
 
 //Insert text b
 
---
+++ toolbar_iframe_script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  // Populate stats
+  var tree_count = 400;
+  $('#treeCount').prepend(tree_count);
+  var pages_left = 48;
+  $('#pagesLeft').prepend(pages_left);
+  pages_left--;
+
+  /*
+  // Snooze functionality
+  $('.cross').hover(
+    function(){
+      //mouseEnterFunction
+    }
+    function(){
+      //mouseLeaveFunction
+    }
+  );
+  $('.cross').on('click', function(){
+    //$('.bottomBar').fadeTo('fast', 1);
+  });
+  */
+});
