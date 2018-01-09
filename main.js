aecffcf94f5a19eeb992011c991940136f0c99e9
Michael Yang <michaelyang182@gmail.com>
2018-01-09 21:49:25 +1100

    added kill toolbar button in popup.html

--- main.js
+++ main.js
@@ -3,28 +3,12 @@
 var height = '70px';
 var iframe = document.createElement('iframe');
 iframe.src = chrome.extension.getURL('toolbar.html');
-iframe.id = 'myOwnCustomFirstToolbar1234'
+iframe.id = 'iframe';
+iframe.name = 'iframe';
 iframe.style.height = height;
-/*iframe.style.width = '100%';
-iframe.style.position = 'fixed';
-iframe.style.bottom = '0';
-iframe.style.left = '0';
-iframe.style.zIndex = '938089';
-iframe.style.backgroundColor = '#F4F4F4';
-iframe.style.border = 'thin solid #CCCCCC'
-*/document.documentElement.appendChild(iframe);﻿
+document.documentElement.appendChild(iframe);﻿
+
+
+
+//Resize document body
 document.body.style.padding = '0px 0px '+height; //set margin height
-
-
-//Insert text b
-
-
-//Insert banner into bottom bar
-// var banner1 = document.createElement('img');
-// banner1.src = chrome.extension.getURL('images/banner_468x60.png');
-// banner1.style.height = '60px';
-// banner1.style.width = '468px';
-// banner1.style.position = 'fixed'
-// banner1.style.bottom = '5px';
-// banner1.style.left = '255px';
-// document.documentElement.iframe.appendChild(banner1);
--- toolbar_iframe_script.js
+++ toolbar_iframe_script.js
@@ -1,23 +1,33 @@
+function acceptXml(xml) {
+    // Process XML
+}
+
+function sendXml(frameName, xml) {
+    var frame = window.parent.frames[frameName];
+    frame.acceptXml(xml);
+}
+
 $(document).ready(function() {
   // Populate stats
   var tree_count = 400;
   $('#treeCount').prepend(tree_count);
   var pages_left = 48;
   $('#pagesLeft').prepend(pages_left);
-  pages_left--;
+  pages_left--; // doesn't work this way
 
-  /*
   // Snooze functionality
   $('.cross').hover(
     function(){
-      //mouseEnterFunction
-    }
+      $('.cross').addClass('mouseOver');
+    },
     function(){
-      //mouseLeaveFunction
+      $('.cross').removeClass('mouseOver');
     }
   );
   $('.cross').on('click', function(){
-    //$('.bottomBar').fadeTo('fast', 1);
+    //$('.bottomBar').hide();
+    parent.document.getElementById(window.name).style.display = 'none';
+    //console.log($('#iframe'));
+    //iframe.contentWindow.postMessage(/*any variable or object here*/, '*');
   });
-  */
 });
