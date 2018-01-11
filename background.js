db9ca706f9a993fe605f929958442a613638faf7
Michael Yang <michaelyang182@gmail.com>
2018-01-11 16:21:37 +1100

    added page count display that live updates across all tabs

---
+++ background.js
@@ -0,0 +1,159 @@
+//Adapted from Page Counter extension by alberto.moratilla@gmail.com  January 2011 and revised on December 2014
+//http://about.me/alberto.moratilla
+
+        var pages_per_tree = 100;
+
+        // initialize variables
+        var page_count = 0;
+        var trees_planted = 0;
+        var pages_left = pages_per_tree;
+        // var session_start = new Date();	    //To save the amount of time browsing this session
+        // var first_initialized = new Date();	    //Check the time when chrome loads
+        // var time_acumulated = 0
+
+        // // localStorage.PC_uses counts the number of times script has run
+        // // localStorage.PC_stored_page_count
+        // // localStorage.PC_first_initialized stores the date first initialized
+        // // localStorage.PC_time_acumulated
+
+        //Initializes statistics - not sure if necessary WIP
+        // if (!localStorage.PC_uses)
+        //     localStorage.PC_uses = 1;
+        // else
+        //     localStorage.PC_uses = (parseInt(localStorage.PC_uses))+1;
+
+
+        //Event when the extension is loaded: whether it is chrome who start or the user who enables the extension
+        window.onload = function() {
+            loadStatistics();
+            badge();
+        };
+
+
+        //Event when extension closes.
+        window.onunload = function () {
+            saveStatistics();
+        };
+
+
+        //Listen for the new page load message from the content script (main.js).
+        chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+            increment_page_count(); // to store new page count to localStorage
+
+            chrome.tabs.query({}, function(tabs) { //query all tabs
+              // obsolete testing code WIP
+              // var number = 0;
+              // var text = 'length '+tabs.length;
+              // for (i=0; i<tabs.length; i++) {
+              //     text = text + ' id'+i+" "+tabs[i].id;
+              // }
+              // // alert(JSON.stringify(text));
+
+              // send updated page count to all tabs
+              var message = {page_count: page_count};
+              for (i=0; i<tabs.length; i++) {
+                  chrome.tabs.sendMessage(tabs[i].id, message, function(response){});
+              }
+            });
+        });
+
+
+
+//================ FUNCTIONS ================//
+
+        // Display latest page count on the extension badge next to omnibar
+        function badge() {
+            var texto = String(page_count);
+            if(page_count>1000)
+            {
+                texto = String(Math.floor(page_count/1000) + "K")
+            }
+
+            chrome.browserAction.setBadgeText({text:texto});
+            chrome.browserAction.setBadgeBackgroundColor({color:[0,255,0,255]});
+        }
+
+
+        //When extension ends, saves the counter and the time.
+        function saveStatistics()
+        {
+            localStorage.PC_stored_page_count =  JSON.stringify( page_count ) ;
+            // localStorage.PC_first_initialized =  JSON.stringify( first_initialized.getTime() ) ;
+            //
+            // //Not sure if this block is necessary WIP
+            // if (!localStorage.PC_time_acumulated)
+            // {
+            //     localStorage.PC_time_acumulated = 0;
+            // }
+            //
+            // var now = new Date();	//current time
+            // localStorage.PC_time_acumulated = JSON.stringify(time_acumulated+(now.getTime()-session_start.getTime()));
+        }
+
+
+        //Load the number of pages visited from the last time
+        function loadStatistics()
+        {
+
+            if (!localStorage.PC_stored_page_count)   //If no data found
+            {
+                page_count = 0;
+                // first_initialized = new Date();
+                // time_acumulated = 0
+            }
+            else  //There are data in localstorage
+            {
+                page_count = JSON.parse(localStorage.PC_stored_page_count);
+                // first_initialized = new Date((Number(JSON.parse(localStorage.PC_first_initialized))));
+                // time_acumulated = (Number(JSON.parse(localStorage.PC_time_acumulated))); //WIP delete outer brackets?
+            }
+        }
+
+
+        //increases the counter and calls badge's refresh function
+        function increment_page_count(){
+            page_count++;
+            badge();
+            saveStatistics();
+        }
+
+
+
+//================ UNUSED FUNCTIONS ================//
+
+        //Time object
+        function tiempo (inicio, fin)
+        {
+            this.inicio = inicio;
+            this.fin = fin;
+        }
+
+        //Returns the time has been accumulated since the start of the program
+        function getAcumulado()
+        {
+            var now = new Date();
+            return (now.getTime() - session_start.getTime()) + time_acumulated;
+
+        }
+
+
+        //Transforms from ms to d:hh:mm:ss format
+        function formatTime(ms)
+        {
+            var dias = Math.floor(ms / (1000*60*60*24));
+            ms = ms - dias*(1000*60*60*24);
+
+            var horas = Math.floor(ms / (1000*60*60));
+            ms = ms - horas*(1000*60*60);
+
+            var minutos = Math.floor(ms / (1000*60));
+            minutos=(minutos <=9)?"0"+minutos:minutos;
+            ms = ms - minutos*(1000*60);
+
+
+            var segundos = Math.floor(ms / (1000));
+            segundos=(segundos <=9)?"0"+segundos:segundos;
+            ms = ms%(1000);
+
+            return (dias +"d " + horas + ":" + minutos + ":" + segundos);
+        }
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-//Create container for bottom bar
+//Create and add container for bottom bar
 var height = '70px';
 var iframe = document.createElement('iframe');
 iframe.src = chrome.extension.getURL('toolbar.html');
 iframe.id = 'iframe';
-iframe.name = 'iframe';
 iframe.style.height = height;
 document.documentElement.appendChild(iframe);﻿
 
@@ -13,33 +12,26 @@ document.body.style.padding = '0px 0px '+height; //set margin height
 //Add a close/snooze button
 var kill = document.createElement('img');
 kill.classList.add('kill');
-kill.name = 'kill';
 kill.src = chrome.extension.getURL('images/cross.png');
 document.documentElement.appendChild(kill);﻿
 
 //Click to kill functionality on click
 kill.addEventListener('click', () => {
-  kill.style.display = 'none';
-  iframe.style.display = 'none';
-  document.body.style.padding = '0';
+    kill.style.display = 'none';
+    iframe.style.display = 'none';
+    document.body.style.padding = '0';
 });
 
-//Mouseover effects
-kill.addEventListener('mouseover', () => {
-  kill.classList.add("mouseOver");
-});
-kill.addEventListener('mouseleave', () => {
-  kill.classList.remove("mouseOver");
-});
+//Add div to display pages left
+var pages_left = document.createElement('div');
+pages_left.classList.add('arbol_test');
+document.documentElement.appendChild(pages_left);﻿
 
+//Send message to background.js to count new page load
+chrome.runtime.sendMessage({message: "new page"}, function(response) {});
 
-//UNUSED CODE BELOW
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  }
-);
+//Listen for the updated page count from background.js even if this isn't the active tab
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    pages_left.innerHTML = 'page count is '+message.page_count;
+    // sendResponse({}); // no need to send response
+});
