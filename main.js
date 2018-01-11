5a869e386efbea002b960e314408ab09d9a37fbc
flfk <flfk@users.noreply.github.com>
2018-01-11 23:56:42 +1100

    created seperate divs for each text element on the bar, requires formatting

--- main.js
+++ main.js
@@ -30,26 +30,55 @@ kill.addEventListener('click', () => {
     document.body.style.padding = '0';
 });
 
-
-
 //Add empty div to display pages left
 var stats_div = document.createElement('div');
 stats_div.classList.add('arbol_test');
 stats_div.classList.add('iframe_element');
 document.documentElement.appendChild(stats_div);﻿
 
+//Add empty div to display Trees Planted Number
+var treesPlantedNum = document.createElement('div');
+// treesPlantedNum.classList.add('arbol_test');
+treesPlantedNum.classList.add('iframe_element');
+treesPlantedNum.classList.add('treesPlantedNum');
+document.documentElement.appendChild(treesPlantedNum);
 
+//Add empty div to display Trees Planted Text
+var treesPlantedText = document.createElement('div');
+// treesPlantedText.classList.add('arbol_test');
+treesPlantedText.classList.add('iframe_element');
+treesPlantedText.classList.add('treesPlantedText');
+document.documentElement.appendChild(treesPlantedText);
+
+//Add empty div to display Pages Left Number
+var pagesLeftNum = document.createElement('div');
+// pagesLeftNum.classList.add('arbol_test');
+pagesLeftNum.classList.add('iframe_element');
+pagesLeftNum.classList.add('pagesLeftNum');
+document.documentElement.appendChild(pagesLeftNum);
+
+//Add empty div to display Trees Planted Text
+var pagesLeftText = document.createElement('div');
+// pagesLeftText.classList.add('arbol_test');
+pagesLeftText.classList.add('iframe_element');
+pagesLeftText.classList.add('pagesLeftText');
+document.documentElement.appendChild(pagesLeftText);
 
 //Send message to background.js to count new page load
 chrome.runtime.sendMessage({message: "new page"}, function(response) {});
 
 //Listen for the updated tree stats from background.js even if this isn't the active tab
 chrome.runtime.onMessage.addListener(function(stats_received, sender, sendResponse){
-    stats_div.innerHTML = treeStats(stats_received); // display stats in the stats_div
+    // stats_div.innerHTML = treeStats(stats_received); // display stats in the stats_div
     // sendResponse({}); // no need to send response
+
+    treesPlantedNum.innerHTML = stats.trees_planted
+    pagesLeftNum.innerHTML = stats.pages_left
+    treesPlantedText.innerHTML = pluralize(stats.trees_planted, "trees", "planted")
+    pagesLeftText.innerHTML = "Hellow world"
+    //pluralize(stats.pages_left, "pages", "to next tree")
+
 });
-
-
 
 // returns a string with the stats trees planted and pages left
 function treeStats (stats) {
@@ -63,5 +92,5 @@ function pluralize (number, units, end) {
     if (number == 1) {
         units = units.slice(0, -1); // slice off the s at the end of the word
     }
-    return number+" "+units+" "+end;
+    return units+" "+end;
 }
