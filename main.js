494c3f33b6b819d17aa8b324edc02ba21e68bab3
Michael Yang <michaelyang182@gmail.com>
2018-02-14 12:57:16 +1100

    first working resize

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Resize document body to make room for iframe
 const HEIGHT = '80px';
-document.body.style.padding = '0px 0px '+HEIGHT; //set padding height
+let viewport_height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+let resized_height = viewport_height - Number(HEIGHT.slice(0,-2));
 const frameURL = 'https://flfk.github.io';
 // const frameURL = 'https://mikey-yang.github.io';
 
@@ -8,17 +9,28 @@ const frameURL = 'https://flfk.github.io';
 var iframe_parent, iframe, kill, treesPlantedNum, treesPlantedText, pagesLeftNum, pagesLeftText;
 populateElements();
 
-
 //Send message to background.js on new page load
 chrome.runtime.sendMessage({message: "new page"}, function(response) {});
+
+// Correctly resize elements that are exactly viewport height
+// Note: DOMContentLoaded event fires too early for pages such as Facebook messenger
+window.addEventListener('load', function() {
+  document.documentElement.style.padding = '0px 0px '+HEIGHT; //set padding height
+  // Downsize full height elements to allow room for the iframe
+  resizeHeight(viewport_height, resized_height);
+});
 
 //Listen for the response from background.js even if this isn't the active tab
 // Note: IF THERE ARE X TABS OPEN IT WILL RUN X TIMES ON EACH TAB
 chrome.runtime.onMessage.addListener(function(message_received, sender, sendResponse){
     if (message_received.currently_snoozed) {
         iframe_parent.classList.add('arbol_hide');
+        document.documentElement.style.padding = 0;
+        resizeHeight(resized_height, viewport_height);
     } else {
         iframe_parent.classList.remove('arbol_hide');
+        document.documentElement.style.padding = '0px 0px '+HEIGHT; //set padding height
+        resizeHeight(viewport_height, resized_height);
     }
 
     // displayStats(message_received); // TODO: delete
@@ -27,6 +39,7 @@ chrome.runtime.onMessage.addListener(function(message_received, sender, sendResp
 //Click to kill functionality
 kill.addEventListener('click', () => {
     iframe_parent.classList.add('arbol_hide');
+    document.documentElement.style.padding = 0;
     //Send message to background.js for snooze start
     chrome.runtime.sendMessage({message: "snooze"}, function(response) {});
 });
@@ -85,6 +98,25 @@ function createElement(tag, classes) {
     }
     return newElement;
 }
+
+/**
+ * Resizes all elements in the DOM with a specific height to another specific
+ * height. Note this is a slow running function as every element in the DOM
+ * is queried and made into a jQuery object
+ * @param  {Number} input_height  Select elements with this height value
+ * @param  {Number} output_height Set their height to this value
+ */
+function resizeHeight(input_height, output_height) {
+  $('*').filter(function() {
+    return $(this).height() == input_height;
+  }).css({
+    'min-height': 0,
+    'height': output_height+'px'
+  });
+}
+
+
+
  // TODO: delete displayStats(), pluralize() and selectPagesLeftIcon()
 function displayStats(message) {
     treesPlantedNum.innerHTML = message.trees_planted;
